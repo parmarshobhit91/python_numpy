b018f662beaf60e8cfe4f22ef3eaebf00ea4a675
parmarshobhit91 <109821693+parmarshobhit91@users.noreply.github.com>
2025-12-11 20:46:59 +0530

    numpy some more methods

--- day5.py
+++ day5.py
@@ -1,2 +1,22 @@
 import numpy as np
 
+a = np.array([10, 20, 30, 40, 50])
+
+b = a[a > 25]
+print(b)
+# Output: [30 40 50]
+
+c = np.where(a > 25, a, -1)
+print(c)
+# Output: [-1 -1 30 40 50]
+
+d = np.where(a > 25, a, 0)
+print(d)
+# Output: [ 0  0 30 40 50]
+
+# np.min
+# np.max
+# np.mean
+# np.median
+# np.std
+# np.var
